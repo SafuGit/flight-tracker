4f94b11449a5948499371d2ad75b940b976d347f
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-03-28 18:51:12 +0600

    first commit

---
+++ flight-tracker-backend/src/main/java/com/safu/flight_tracker_backend/FlightTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safu.flight_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTrackerBackendApplication.class, args);
+	}
+
+}
